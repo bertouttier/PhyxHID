8ffc85ef99400b504c84b36bb2a79d36ad9a00c3
Bert Outtier <outtierbert@gmail.com>
2015-05-06 22:04:34 +0200

    first commit

---
+++ Phyx_hid_device/Debug/example/src/hid_main.d
@@ -0,0 +1,145 @@
+example/src/hid_main.o example/src/hid_main.d: ../example/src/hid_main.c \
+ /Users/bertouttier/development/workspaces/LPCXpresso/phyx_11u35_board_lib/inc/board.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/chip.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/lpc_types.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/sys_config.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/cmsis.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/cmsis_11uxx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/core_cm0.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/core_cmInstr.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/core_cmFunc.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/pmu_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/fmc_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/sysctl_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/clock_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/iocon_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/timer_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/uart_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/ring_buffer.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/wwdt_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/ssp_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/romapi_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/error.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/adc_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/gpio_11xx_1.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/gpiogroup_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/pinint_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/i2c_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/i2c_common_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/i2cm_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd_11xx.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/phyx_11u35_board_lib/inc/board_api.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/lpc_types.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/Phyx_hid_device/example/inc/app_usbd_cfg.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/error.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_rom_api.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_hw.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_core.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_mscuser.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_msc.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_dfuuser.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_dfu.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_hiduser.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_hid.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_cdcuser.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_cdc.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/Phyx_hid_device/example/inc/hid_keyboard.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/Phyx_hid_device/example/inc/app_usbd_cfg.h \
+ /Users/bertouttier/development/workspaces/LPCXpresso/Phyx_hid_device/example/inc/ms_timer.h
+
+/Users/bertouttier/development/workspaces/LPCXpresso/phyx_11u35_board_lib/inc/board.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/chip.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/lpc_types.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/sys_config.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/cmsis.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/cmsis_11uxx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/core_cm0.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/core_cmInstr.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/core_cmFunc.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/pmu_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/fmc_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/sysctl_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/clock_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/iocon_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/timer_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/uart_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/ring_buffer.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/wwdt_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/ssp_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/romapi_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/error.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/adc_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/gpio_11xx_1.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/gpiogroup_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/pinint_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/i2c_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/i2c_common_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/i2cm_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd_11xx.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/phyx_11u35_board_lib/inc/board_api.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/lpc_types.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/Phyx_hid_device/example/inc/app_usbd_cfg.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/error.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_rom_api.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_hw.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_core.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_mscuser.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_msc.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_dfuuser.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_dfu.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_hiduser.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_hid.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_cdcuser.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/usbd/usbd_cdc.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/Phyx_hid_device/example/inc/hid_keyboard.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/Phyx_hid_device/example/inc/app_usbd_cfg.h:
+
+/Users/bertouttier/development/workspaces/LPCXpresso/Phyx_hid_device/example/inc/ms_timer.h:
---
+++ phyx_11u35_board_lib/src/board.c
@@ -0,0 +1,150 @@
+/*
+ * @brief NXP LPCXpresso 11U37H board file
+ *
+ * @note
+ * Copyright(C) NXP Semiconductors, 2013
+ * All rights reserved.
+ *
+ * @par
+ * Software that is described herein is for illustrative purposes only
+ * which provides customers with programming information regarding the
+ * LPC products.  This software is supplied "AS IS" without any warranties of
+ * any kind, and NXP Semiconductors and its licensor disclaim any and
+ * all warranties, express or implied, including all implied warranties of
+ * merchantability, fitness for a particular purpose and non-infringement of
+ * intellectual property rights.  NXP Semiconductors assumes no responsibility
+ * or liability for the use of the software, conveys no license or rights under any
+ * patent, copyright, mask work right, or any other intellectual property rights in
+ * or to any products. NXP Semiconductors reserves the right to make changes
+ * in the software without notification. NXP Semiconductors also makes no
+ * representation or warranty that such application will be suitable for the
+ * specified use without further testing or modification.
+ *
+ * @par
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation is hereby granted, under NXP Semiconductors' and its
+ * licensor's relevant copyrights in the software, without fee, provided that it
+ * is used in conjunction with NXP Semiconductors microcontrollers.  This
+ * copyright, permission, and disclaimer notice must appear in all copies of
+ * this code.
+ */
+
+#include "board.h"
+#include "retarget.h"
+
+/*****************************************************************************
+ * Private types/enumerations/variables
+ ****************************************************************************/
+#define SetBit(port, bit) port|= (1<<bit)
+#define ClearBit(port, bit) port&= ~(1<<bit)
+#define BUTTONS_ENCA_GPIO_PORT_NUM			0
+#define BUTTONS_ENCA_GPIO_BIT_NUM			4
+#define BUTTONS_ENCB_GPIO_PORT_NUM			0
+#define BUTTONS_ENCB_GPIO_BIT_NUM			5
+#define BUTTONS_ENCC_GPIO_PORT_NUM			0
+#define BUTTONS_ENCC_GPIO_BIT_NUM			1
+#define RIGHT_SPIN 							0x01
+#define LEFT_SPIN 							0xff
+
+/*****************************************************************************
+ * Public types/enumerations/variables
+ ****************************************************************************/
+
+/* System oscillator rate and clock rate on the CLKIN pin */
+const uint32_t OscRateIn = 12000000;
+const uint32_t ExtRateIn = 0;
+volatile uint8_t bufEnc = 0; //encoder buffer
+
+/*****************************************************************************
+ * Private functions
+ ****************************************************************************/
+
+/*****************************************************************************
+ * Public functions
+ ****************************************************************************/
+
+/* Set up and initialize all required blocks and functions related to the
+   board hardware */
+void Board_Init(void)
+{
+	/* Initialize GPIO */
+	Chip_GPIO_Init(LPC_GPIO);
+}
+
+/* Initialize buttons on the board */
+void Board_Buttons_Init(void)
+{
+	//enable pull-ups
+	Chip_IOCON_PinMuxSet(LPC_IOCON, BUTTONS_ENCA_GPIO_PORT_NUM, BUTTONS_ENCA_GPIO_BIT_NUM,
+					(IOCON_FUNC0 | IOCON_MODE_PULLUP | IOCON_DIGMODE_EN));
+	Chip_IOCON_PinMuxSet(LPC_IOCON, BUTTONS_ENCB_GPIO_PORT_NUM, BUTTONS_ENCB_GPIO_BIT_NUM,
+					(IOCON_FUNC0 | IOCON_MODE_PULLUP | IOCON_DIGMODE_EN));
+	Chip_IOCON_PinMuxSet(LPC_IOCON, BUTTONS_ENCC_GPIO_PORT_NUM, BUTTONS_ENCC_GPIO_BIT_NUM,
+				(IOCON_FUNC0 | IOCON_MODE_PULLUP | IOCON_DIGMODE_EN));
+
+	//set as input pins
+	Chip_GPIO_SetPinDIRInput(LPC_GPIO, BUTTONS_ENCA_GPIO_PORT_NUM, BUTTONS_ENCA_GPIO_BIT_NUM);
+	Chip_GPIO_SetPinDIRInput(LPC_GPIO, BUTTONS_ENCB_GPIO_PORT_NUM, BUTTONS_ENCB_GPIO_BIT_NUM);
+	Chip_GPIO_SetPinDIRInput(LPC_GPIO, BUTTONS_ENCC_GPIO_PORT_NUM, BUTTONS_ENCC_GPIO_BIT_NUM);
+}
+
+/* Get button status */
+uint32_t Buttons_GetStatus(void)
+{
+	uint8_t ret = NO_BUTTON_PRESSED;
+
+	uint8_t encodeState = Buttons_GetStateEncoder();
+
+	if (encodeState == RIGHT_SPIN)
+	{
+		ret |= BUTTONS_RIGHT;
+	}
+
+	if (encodeState == LEFT_SPIN)
+	{
+		ret |= BUTTONS_LEFT;
+	}
+
+	if (!Chip_GPIO_GetPinState(LPC_GPIO, BUTTONS_ENCC_GPIO_PORT_NUM, BUTTONS_ENCC_GPIO_BIT_NUM))
+	{
+		ret |= BUTTONS_ENCC;
+	}
+
+	return ret;
+}
+
+/* poll button status */
+void Poll_Buttons(void)
+{
+	static uint8_t stateEnc;
+	uint8_t tmp;
+	uint8_t currentState = 0;
+
+	//check state of pins
+	if (Chip_GPIO_GetPinState(LPC_GPIO, BUTTONS_ENCA_GPIO_PORT_NUM, BUTTONS_ENCA_GPIO_BIT_NUM))
+	{
+		SetBit(currentState,0);
+	}
+
+	if (Chip_GPIO_GetPinState(LPC_GPIO, BUTTONS_ENCC_GPIO_PORT_NUM, BUTTONS_ENCC_GPIO_BIT_NUM))
+	{
+		SetBit(currentState,1);
+	}
+
+	tmp = stateEnc;
+	if (currentState == (tmp & 0x03)) return; // 00000011b
+
+	tmp = (tmp<<2)|currentState;
+	stateEnc = tmp;
+
+	if (tmp == 0xE1) bufEnc = LEFT_SPIN; //11100001b
+	if (tmp == 0xD2) bufEnc = RIGHT_SPIN; //11010010b
+	return;
+}
+
+uint8_t Buttons_GetStateEncoder(void)
+{
+	uint8_t tmp = bufEnc;
+	bufEnc = 0;
+	return tmp;
+}
