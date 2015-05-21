e997897dc684f4052362395ed726def11c8630f3
Bert Outtier <outtierbert@gmail.com>
2015-05-21 21:01:54 +0200

    Remove unnecessary files

--- Phyx_hid_device/Debug/example/src/cr_startup_lpc11xx.d
+++
@@ -1,4 +0,0 @@
-example/src/cr_startup_lpc11xx.d: ../example/src/cr_startup_lpc11xx.c \
- /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/sys_config.h
-
-/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/sys_config.h:
--- lpc_chip_11uxx_lib/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/ring_buffer.h \
- /Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/lpc_types.h
-
-/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/ring_buffer.h:
-
-/Users/bertouttier/development/workspaces/LPCXpresso/lpc_chip_11uxx_lib/inc/lpc_types.h:
